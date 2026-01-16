fcba2002575761af848c672692fdd2efafadd70d
Mike McKague <m.mckague19@gmail.com>
2026-01-16 14:35:33 -0500

    added properties

---
+++ scripts/properties/_properties.py
@@ -0,0 +1,14 @@
+properties = {
+    "rho_mi": 1,
+    "rho_sep": 0.75,
+    "D": 1.68e-9,
+    "mu": 1e-3,
+    "K": 1e-11,
+    "Km": 1.68e-9,
+    "P_in": 1,
+    "P_out": 0,
+    "cf": 100,  # mol/m3
+    "V_cell": 1.0,
+    "mu_att": 0,
+    "Ca": 145 * 100 ** 3  # (145 F/mL, Guyes 2017)
+}
