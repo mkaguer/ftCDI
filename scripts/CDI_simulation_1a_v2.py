53542ac1a943cbf533771b457fb7a5b8a4cf80f6
Mike McKague <m.mckague19@gmail.com>
2026-01-24 14:00:00 -0500

    Begin adding mass transport of v2 cdi sim

--- scripts/CDI_simulation_1a_v2.py
+++ scripts/CDI_simulation_1a_v2.py
@@ -4,9 +4,13 @@ import collection
 import models.misc as mods
 from scipy.integrate import solve_ivp
 from scipy.sparse.linalg import cg, LinearOperator
+import properties as prpts
 import pyamg
 import time
 import network
+import models
+
+op.visualization.set_mpl_style()
 
 # set dimensions
 l_sep = 8e-5  # should be 190um (Guyes)
@@ -38,93 +42,138 @@ throats = op.topotools.find_interface_throats(net,
                                               P2=net.pores("perforated"))
 net["throat.perforated"][throats] = False
 net["throat.macropore"][throats] = True
-ts = np.zeros(net.Nt).astype(bool)
 
-# re-label separator throats as macropore
-throats = net["throat.separator"]
-net["throat.macropore"][throats] = True
+# calculate throats coords
+conns = net["throat.conns"]
+coords = net["pore.coords"]
+t_coords = np.diff(coords[conns], axis=1).squeeze(1)/2 + coords[conns[:, 0]]
+net["throat.coords"] = t_coords
 
-# re-label separator pores as macropore pores
-pores = net["pore.separator"]
-net["pore.macropore"][pores] = True
+# set pore thickness for perforated pores (i.e. cylinders)
+net["pore.thickness"] = spacing
 
-# re-label perforated pores as macropore pores
-pores = net["pore.perforated"]
-net["pore.macropore"][pores] = True
+# add geometry models to ALL domains
+geo_mods_micro = collection.geometry.micropore.spheres_and_cylinders
+geo_mods_macro = collection.geometry.macropore.spheres_and_cylinders
+geo_mods_separ = collection.geometry.separator.continuum
+geo_mods_perfo = collection.geometry.macropore.intersecting_cylinders
+net.add_model_collection(models=geo_mods_macro,
+                         domain="macropore",
+                         regen_mode="normal")
+net.add_model_collection(models=geo_mods_micro,
+                         domain="micropore",
+                         regen_mode="normal")
+net.add_model_collection(models=geo_mods_separ,
+                         domain="separator",
+                         regen_mode="normal")
+net.add_model_collection(models=geo_mods_perfo,
+                         domain="perforated",
+                         regen_mode="normal")
 
-# re-label perforated throats as macropore throats
-throats = net["throat.perforated"]
-net["throat.macropore"][throats] = True
+# retrieve properties
+rho_sep = prpts.properties["rho_sep"]
+rho_mi = prpts.properties["rho_mi"]
+V_cell = prpts.properties["V_cell"]
+Ca = prpts.properties["Ca"]
+mu_att = prpts.properties["mu_att"]
+P_in = prpts.properties["P_in"]  # FIXME: not using these currently
+P_out = prpts.properties["P_out"]
 
-# delete unecessary labels
-del net["throat.separator"]
-del net["pore.perforated"]
-del net["throat.perforated"]
+# FIXME: this is SLOW, Fix!
+# calculate effective volume
+eff_vol = models.effective_volume.bcc(network=net,
+                                      rho_sep=rho_sep, rho_mi=rho_mi)
+net["pore.effective_volume"] = eff_vol
 
-print(net)
-
-# add geometry to macropore
-net["pore.diameter@macropore"] = 0.5*spacing
-geo_mods = op.models.collections.geometry.spheres_and_cylinders.copy()
-del geo_mods["pore.diameter"], geo_mods["pore.seed"], geo_mods["pore.max_size"]
-net.add_model_collection(models=geo_mods, domain="macropore")
-
-# add geometry to micropores
-net["pore.diameter@micropore"] = 0.25*spacing
-del geo_mods["throat.volume"]  # delete so doesn't get added to eff macro vol
-net.add_model_collection(models=geo_mods, domain="micropore")
-
-# regenerate models
-net.regenerate_models()
-
-# FIXME: will need new model here when we scale!
-# calculate effective volume of micropores, this is hard coded!
-Vp = net["pore.volume@macropore"][0]
-Vt = net["throat.volume@macropore"][0]
-net["pore.effective_volume@micropore"] = 0.2*(spacing**3 - Vp - Vt*2)  # FIXME: multiply by porosity
-net["throat.volume@micropore"] = 0
-eff_vol_mod = op.models.geometry.pore_volume.effective
-net.add_model(propname="pore.effective_volume",
-              model=eff_vol_mod,
-              domain="macropore")
+'''
+# save as xdmf for visualization
+net["throat.radius"] = net["throat.diameter"]/2
+D = net["pore.diameter"].copy()
+D[net.pores("micropore")] = spacing/10
+net["pore.diameter"] = D
+project = net.project
+op.io.project_to_xdmf(project,
+                      filename="../paraview/" + "CDI_simulation_1a" + ".xdmf")
+'''
 
 # create phase object
-phase = op.phase.Phase(net)
+phase = op.phase.Phase(network=net)
+phase.settings["auto_interpolate"] = False
 
-# add properties to phase object
-phase["pore.concentration"] = 100
-phase["pore.potential@cathode"] = -0.5
-phase["pore.potential@anode"] = 0.5
-phase["pore.potential@separator"] = 0.0
-phase["pore.electrode_potential@cathode"] = -0.5
-phase["pore.electrode_potential@anode"] = 0.5
-phase["pore.electrode_potential@separator"] = 0.0
-phase["pore.attraction_term"] = 0
-phase["pore.capacitance"] = 145 * 1e6
-phase["pore.surface_area"] = 1
-phase["pore.diffusivity@macropore"] = 1.68e-9
-phase["pore.diffusivity@micropore"] = 6e-11
-phase["pore.temperature"] = 298
+# initialize concentration, pressure, and potential
+cf = prpts.properties["cf"]  # 100  # mM
+phase["pore.concentration"] = np.ones(net.Np)*cf
+phase["pore.pressure"] = np.ones(net.Np)
 
-# add physics models for macropore
-phys_mods_ma = collection.physics.macropore
-del phys_mods_ma["throat.diffusive_conductance"]
-del phys_mods_ma["throat.hydraulic_conductance"]
-del phys_mods_ma["throat.mass_conductance"]
-phys_mods_ma["throat.conductivity"]["concentration"] = "throat.concentration"
-phys_mods_ma["pore.conductivity"]["concentration"] = "pore.concentration"
-phase.add_model_collection(models=phys_mods_ma, domain="macropore")
+# add phase models to ALL domains
+phase.add_model_collection(models=collection.phase.macropore,
+                           domain="macropore",
+                           regen_mode="normal")
+phase.add_model_collection(models=collection.phase.micropore,
+                           domain="micropore",
+                           regen_mode="normal")
+phase.add_model_collection(models=collection.phase.separator,
+                           domain="separator",
+                           regen_mode="normal")
+phase.add_model_collection(models=collection.phase.macropore,
+                           domain="perforated",
+                           regen_mode="normal")
 
-# add physics models for micropore
-phys_mods_mi = collection.physics.micropore
-del phys_mods_mi["throat.mass_conductance"]
-del phys_mods_mi["throat.hydraulic_conductance"]
-phys_mods_mi["throat.conductivity"]["concentration"] = "throat.concentration"
-phys_mods_mi["pore.conductivity"]["concentration"] = "pore.concentration"
-phase.add_model_collection(models=phys_mods_mi, domain="micropore")
+# interpolate to find throat concentration and temperature
+phase.add_model(propname="throat.concentration",
+                model=op.models.misc.from_neighbor_pores,
+                prop="pore.concentration",
+                mode="mean")
+phase.add_model(propname="throat.temperature",
+                model=op.models.misc.from_neighbor_pores,
+                prop="pore.temperature",
+                mode="mean")
 
-# regenerate models
-phase.regenerate_models()
+# set "old" concentrations
+phase["pore.concentration_old"] = phase["pore.concentration"].copy()
+phase["throat.concentration_old"] = phase["throat.concentration"].copy()
+
+# add physics models
+phase.add_model_collection(models=collection.physics.macropore,
+                           domain="perforated",
+                           regen_mode="normal")
+phase.add_model_collection(models=collection.physics.macropore,
+                           domain="macropore",
+                           regen_mode="normal")
+phase.add_model_collection(models=collection.physics.micropore,
+                           domain="micropore",
+                           regen_mode="normal")
+phase.add_model_collection(models=collection.physics.separator,
+                           domain="separator",
+                           regen_mode="normal")
+
+# set potential
+phase["pore.potential@cathode"] = -V_cell/2
+phase["pore.potential@anode"] = V_cell/2
+phase["pore.potential@separator"] = 0
+
+# set electrode potential
+phase["pore.electrode_potential@cathode"] = -V_cell/2
+phase["pore.electrode_potential@anode"] = V_cell/2
+phase["pore.electrode_potential@separator"] = 0
+
+# set attraction term
+phase["pore.attraction_term@macropore"] = 0
+phase["pore.attraction_term@micropore"] = mu_att
+phase["pore.attraction_term@separator"] = 0
+phase["pore.attraction_term@perforated"] = 0
+
+# set capacitance
+phase["pore.capacitance@macropore"] = 1
+phase["pore.capacitance@micropore"] = Ca
+phase["pore.capacitance@separator"] = 1
+phase["pore.capacitance@perforated"] = 1
+
+# set surface area
+phase["pore.surface_area@macropore"] = 1
+phase["pore.surface_area@micropore"] = 1
+phase["pore.surface_area@separator"] = 1
+phase["pore.surface_area@perforated"] = 1
 
 # add donnan potential model
 phase["pore.donnan_potential_old"] = np.zeros(net.Np)  # guess
@@ -162,14 +211,10 @@ ct._apply_sources()
 A = ct.A
 b = ct.b
 
-# get dimensions
-Ns = np.sum(net["pore.macropore"])
-Nt = np.sum(net["pore.micropore"])
-
 # break up b into steady and transient parts
-mask_s = net["pore.macropore"]
-b_s = b[mask_s]
-b_t = b[~mask_s]  # FIXME: b changes so we have to get new every time
+mask_t = net["pore.micropore"]
+b_s = b[~mask_t]
+b_t = b[mask_t]  # FIXME: b changes so we have to get new every time
 
 # conver A to csr
 start = time.time()
@@ -178,8 +223,8 @@ stop = time.time()
 print(f"Time to convert to csr: {stop - start}s")
 
 # create masks
-idx_s = np.where(mask_s)[0]  # indices of macropores (steady)
-idx_t = np.where(~mask_s)[0]
+idx_s = np.where(~mask_t)[0]  # indices of macropores (steady)
+idx_t = np.where(mask_t)[0]
 
 A_ss = A_csr[idx_s, :][:, idx_s]  # steady-steady
 A_st = A_csr[idx_s, :][:, idx_t]  # steady-transient
@@ -233,7 +278,7 @@ def rhs_charge(t, x):
 # perform time stepping
 t0 = 0
 dt = dt
-tf = 0.01
+tf = 0.1
 for t in np.arange(t0 + dt, tf+dt, dt):
     print(f"Simulation time: {t}s")
     # get phi0
@@ -249,8 +294,8 @@ for t in np.arange(t0 + dt, tf+dt, dt):
     stop = time.time()
     print(f"Time: {stop - start}s")
     # update potential
-    phase["pore.potential@micropore"] = phi_t
-    phase["pore.potential@macropore"] = phi_s
+    phase["pore.potential"][mask_t] = phi_t
+    phase["pore.potential"][~mask_t] = phi_s
     # update donnan potential old
     phase["pore.donnan_potential_old"] = phase["pore.donnan_potential"].copy()
     # regenerate phase models
@@ -259,4 +304,4 @@ for t in np.arange(t0 + dt, tf+dt, dt):
     ct._apply_BCs()
     ct._apply_sources()
     # get new b_t
-    b_t = b[~mask_s]
+    b_t = b[mask_t]
