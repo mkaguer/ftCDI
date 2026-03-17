727d24826e971dc033381079b7bd81c0a719b44f
Mike <m.mckague19@gmail.com>
2026-03-17 10:36:29 -0400

    wrote from_neighbor_throats in jax

--- scripts/pnmlib/models/misc/_misc.py
+++ scripts/pnmlib/models/misc/_misc.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import jax.experimental.sparse as sprs
 
 __all__ = ["difference",
            "constant",
@@ -6,7 +7,8 @@ __all__ = ["difference",
            "fraction",
            "from_neighbor_pores",
            "conductivity",
-           "find_neighbor_throats"]
+           "find_neighbor_throats",
+           "from_neighbor_throats"]
 
 
 def difference(network, props):
@@ -206,3 +208,157 @@ def find_neighbor_throats(network, pores):
     throats = jnp.any(jnp.isin(conns, pores), axis=1)
 
     return throats
+
+
+def from_neighbor_throats(network, prop, mode='min', ignore_nans=True):
+    r"""
+    Adopt a value from the values found in neighboring throats
+
+    Parameters
+    ----------
+    target : Base
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
+    prop : str
+        The dictionary key of the array containing the throat property to be
+        used in the calculation.
+    mode : str
+        Controls how the pore property is calculated. The default value is
+        'min'. Options are:
+
+            ===========  =====================================================
+            mode         meaning
+            ===========  =====================================================
+            'min'        Returns the value of the minimum property of the
+                         neighboring throats
+            'max'        Returns the value of the maximum property of the
+                         neighboring throats
+            'mean'       Returns the value of the mean property of the
+                         neighboring throats
+            'sum'        Returns the sum of the property of the neighboring
+                         throats
+            ===========  =====================================================
+
+    Returns
+    -------
+    value : ndarray
+        Array containing customized values based on those of adjacent throats.
+
+    """
+    Np = len(network["pore.coords"])
+    Nt = len(network["throat.conns"])
+    data = network[prop]
+    nans = jnp.isnan(data)
+    im = _create_incidence_matrix(network)
+    if mode == 'min':
+        if ignore_nans:
+            data[nans] = jnp.inf
+        values = jnp.ones((Np, ))*jnp.inf
+        jnp.minimum.at(values, im.row, data[im.col])
+    if mode == 'max':
+        if ignore_nans:
+            data[nans] = -jnp.inf
+        values = jnp.ones((network.Np, ))*-jnp.inf
+        jnp.maximum.at(values, im.row, data[im.col])
+    if mode == 'mean':
+        if ignore_nans:
+            data = data.at[nans].set(0)
+        values = jnp.zeros((Np, ))
+        values = jnp.add.at(values, im.row, data[im.col], inplace=False)
+        counts = jnp.zeros((Np, ))
+        counts = jnp.add.at(counts, im.row, jnp.ones((Nt, ))[im.col], inplace=False)
+        if ignore_nans:
+            counts = jnp.subtract.at(counts, im.row, nans[im.col], inplace=False)
+        values = values/counts
+    if mode == 'sum':
+        if ignore_nans:
+            data[nans] = 0
+        values = jnp.zeros((network.Np, ))
+        jnp.add.at(values, im.row, data[im.col])
+    return values
+
+
+def _create_incidence_matrix(network, weights=None, fmt='coo',
+                            drop_zeros=False):
+    r"""
+    Creates a weighted incidence matrix in the desired sparse format
+
+    Parameters
+    ----------
+    weights : array_like, optional
+        An array containing the throat values to enter into the matrix
+        (in graph theory these are known as the 'weights'). If
+        omitted, ones are used to create a standard incidence matrix
+        representing connectivity only.
+    fmt : str, default is 'coo'
+        The sparse storage format to return. Options are:
+            **'coo'** : This is the native format of OpenPNM's data
+            **'lil'** : Enables row-wise slice of the matrix
+            **'csr'** : Favored by most linear algebra routines
+            **'dok'** : Enables subscript access of locations
+    drop_zeros : bool, default is ``False``
+        If ``True``, applies the ``eliminate_zeros`` method of the sparse
+        array to remove all zero locations.
+
+    Returns
+    -------
+    sparse_array
+        An incidence matrix in the specified sparse format
+
+    Notes
+    -----
+    The incidence matrix is a cousin to the adjacency matrix, and used
+    by OpenPNM for finding the throats connected to a give pore or set
+    of pores. Specifically, an incidence matrix has Np rows and Nt
+    columns, and each row represents a pore, containing non-zero
+    values at the locations corresponding to the indices of the
+    throats connected to that pore. The ``weights`` argument indicates
+    what value to place at each location, with the default being 1's
+    to simply indicate connections. Another useful option is throat
+    indices, such that the data values on each row indicate which
+    throats are connected to the pore, though this is redundant as it
+    is identical to the locations of non-zeros.
+
+    Examples
+    --------
+    >>> import openpnm as op
+    >>> pn = op.network.Cubic(shape=[5, 5, 5])
+    >>> weights = np.random.rand(pn.num_throats(), ) < 0.5
+    >>> im = pn.create_incidence_matrix(weights=weights, fmt='csr')
+
+    """
+    Np = len(network["pore.coords"])
+    Nt = len(network["throat.conns"])
+    # Check if provided data is valid
+    if weights is None:
+        weights = jnp.ones((Nt,), dtype=int)
+    if jnp.shape(weights)[0] == Nt:
+        weights = jnp.append(weights, weights)
+    elif jnp.shape(weights)[0] != 2*Nt:
+        raise Exception('Received dataset of incorrect length')
+
+    conn = network['throat.conns']
+    row = conn[:, 1]
+    row = jnp.append(row, conn[:, 0])
+    col = jnp.arange(Nt)
+    col = jnp.append(col, col)
+
+    # temp = sprs.coo.coo_matrix((weights, (row, col)), (Np, Nt))
+    # indices = jnp.stack([row, col], axis=1)
+    temp = sprs.COO((weights, row, col), shape=(Np, Nt))
+
+    if drop_zeros:
+        temp.eliminate_zeros()
+
+    # Convert to requested format
+    if fmt == 'coo':
+        pass  # temp is already in coo format
+    elif fmt == 'csr':
+        temp = temp.tocsr()
+    elif fmt == 'lil':
+        temp = temp.tolil()
+    elif fmt == 'dok':
+        temp = temp.todok()
+
+    return temp
